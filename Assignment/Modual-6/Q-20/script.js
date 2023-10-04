82d09f0cd297b3bc9fac0f37bb120759a9739034
Gaurav Nakum <135405398+Gauravnakum12@users.noreply.github.com>
2023-10-04 22:35:36 +0530

    04-10-2023

---
+++ Assignment/Modual-6/Q-20/script.js
@@ -0,0 +1,23 @@
+let in1 = document.getElementById('v1').value;
+        let in2 = document.getElementById('v2').value;
+
+        function add() {
+            ans.value = parseInt(v1.value) + parseInt(v2.value);
+            document.getElementById("ans").innerHTML = " your answer is:" + ans.value;
+        }
+        function sub() {
+            ans.value = parseInt(v1.value) - parseInt(v2.value);
+            document.getElementById("ans").innerHTML = " your answer is:" + ans.value;
+        }
+        function mult() {
+            ans.value = parseInt(v1.value) * parseInt(v2.value);
+            document.getElementById("ans").innerHTML = " your answer is:" + ans.value;
+        }
+        function divi() {
+            ans.value = parseInt(v1.value) / parseInt(v2.value);
+            document.getElementById("ans").innerHTML = " your answer is:" + ans.value;
+        }
+        function percent() {
+            ans.value = (parseInt(v1.value) / parseInt(v2.value)) * 100;
+            document.getElementById("ans").innerHTML = " your answer is:" + ans.value + '%';
+        }
