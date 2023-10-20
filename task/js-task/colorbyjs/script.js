191153502e4205dd4e2229516b4f0682813cadc9
Gaurav Nakum <135405398+Gauravnakum12@users.noreply.github.com>
2023-10-20 16:34:10 +0530

    20-10-2023

--- task/js-task/colorbyjs/script.js
+++ task/js-task/colorbyjs/script.js
@@ -1,23 +1,23 @@
 // ------------------background-color-------------------------------
 
-    let a = document.getElementById("in-1");
-    function bgcolour() {
-        document.getElementById('bg').style.background = a.value;
-    }
+let a = document.getElementById("in-1");
+function bgcolour() {
+    document.getElementById('bg').style.background = a.value;
+}
 
 // ----------------------------font-color-----------------------------
-    function text() {
-        document.getElementById('bg').style.color = 'red';
-    }
-    function text1() {
-        document.getElementById('bg').style.color = 'Green';
-    }
-    function text2() {
-        document.getElementById('bg').style.color = 'blue';
-    }
-    function text3() {
-        document.getElementById('bg').style.color = 'purple';
-    }
-    function text4() {
-        document.getElementById('bg').style.color = 'black';
-    }
+function tex() {
+    document.getElementById('bg').style.color = 'red';
+}
+function text1() {
+    document.getElementById('bg').style.color = 'Green';
+}
+function text2() {
+    document.getElementById('bg').style.color = 'blue';
+}
+function text3() {
+    document.getElementById('bg').style.color = 'purple';
+}
+function text4() {
+    document.getElementById('bg').style.color = 'black';
+}
---
+++ task/js-task/js-slider/script.js
@@ -0,0 +1,29 @@
+document.getElementById('im');
+function next() {
+    if (im.src == 'https://www.100mbsports.com/wp-content/uploads/2020/11/Rohit-209-e1614250334502.jpeg') {
+        im.src = 'https://imgeng.jagran.com/images/2023/sep/SD1693916433356.jpg';
+    }
+    else if (im.src == 'https://imgeng.jagran.com/images/2023/sep/SD1693916433356.jpg') {
+        im.src = 'https://s01.sgp1.cdn.digitaloceanspaces.com/article/181748-evlugsbvng-1666543768.jpg';
+    }
+    else if (im.src == 'https://s01.sgp1.cdn.digitaloceanspaces.com/article/181748-evlugsbvng-1666543768.jpg') {
+        im.src = 'https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200/lsci/db/PICTURES/CMS/156600/156689.jpg';
+    }
+    else if (im.src == 'https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200/lsci/db/PICTURES/CMS/156600/156689.jpg') {
+        im.src = 'https://imgeng.jagran.com/images/2023/may/Magic1685393880405.jpg';
+    }
+}
+function pre() {
+    if (im.src == 'https://imgeng.jagran.com/images/2023/sep/SD1693916433356.jpg') {
+        im.src = 'https://www.100mbsports.com/wp-content/uploads/2020/11/Rohit-209-e1614250334502.jpeg'
+    }
+    else if (im.src == 'https://s01.sgp1.cdn.digitaloceanspaces.com/article/181748-evlugsbvng-1666543768.jpg') {
+        im.src = 'https://imgeng.jagran.com/images/2023/sep/SD1693916433356.jpg'
+    }
+    else if (im.src == 'https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200/lsci/db/PICTURES/CMS/156600/156689.jpg') {
+        im.src = 'https://s01.sgp1.cdn.digitaloceanspaces.com/article/181748-evlugsbvng-1666543768.jpg'
+    }
+    else if (im.src == 'https://imgeng.jagran.com/images/2023/may/Magic1685393880405.jpg') {
+        im.src = 'https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_1200/lsci/db/PICTURES/CMS/156600/156689.jpg'
+    }
+}
